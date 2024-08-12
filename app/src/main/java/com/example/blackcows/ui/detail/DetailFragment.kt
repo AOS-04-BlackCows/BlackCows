6fa9cd50e5121d2afe6949a80b6d0ff552feaf4c
MunKiJeon <jmk070994@gmail.com>
2024-08-13 02:58:06 +0900

    FragmentSetting

--- app/src/main/java/com/example/blackcows/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/blackcows/ui/detail/DetailFragment.kt
@@ -1,4 +1,4 @@
-package com.example.blackcows.ui.dashboard
+package com.example.blackcows.ui.detail
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.blackcows.databinding.FragmentDashboardBinding
+import com.example.blackcows.databinding.FragmentDetailBinding
 
-class DashboardFragment : Fragment() {
+class DetailFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentDetailBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val detailViewModel =
+            ViewModelProvider(this).get(DetailViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        detailViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/blackcows/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/blackcows/ui/detail/DetailViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.blackcows.ui.dashboard
+package com.example.blackcows.ui.detail
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class DetailViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Detail Fragment"
     }
     val text: LiveData<String> = _text
 }
