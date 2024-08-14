4cbba7adf54e349be1c4c1f197cf496f7b1c3b49
이화민 <whchzh1313@gmail.com>
2024-08-14 20:29:08 +0900

    feat: SearchFragment에 유튜브 api를 통한 검색 제작

---
+++ app/src/main/java/com/example/blackcows/ui/adapter/PublicListAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.blackcows.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.blackcows.ListItem
+import com.example.blackcows.R
+
+class PublicListAdapter : ListAdapter<ListItem.VideoItem, PublicListAdapter.SearchViewHolder>(object : DiffUtil.ItemCallback<ListItem.VideoItem>() {
+    override fun areItemsTheSame(oldItem: ListItem.VideoItem, newItem: ListItem.VideoItem): Boolean {
+        return oldItem.thumbnail == newItem.thumbnail && oldItem.title == newItem.title
+    }
+
+    override fun areContentsTheSame(oldItem: ListItem.VideoItem, newItem: ListItem.VideoItem): Boolean {
+        return oldItem == newItem
+    }
+}) {
+    interface ItemClick {
+        fun onClick(view: View, position: Int)
+    }
+
+    var itemClick : ItemClick? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.search_list_item, parent, false)
+        return SearchViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        Glide.with(holder.listThumbnail.context)
+            .load(getItem(position).thumbnail)
+            .into(holder.listThumbnail)
+        holder.listTitle.text = getItem(position).title
+        holder.listWriter.text = getItem(position).channelTitle
+
+    }
+
+    inner class SearchViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val listThumbnail: ImageView = view.findViewById(R.id.search_thumb)
+        val listTitle: TextView = view.findViewById(R.id.search_title)
+        val listWriter: TextView = view.findViewById(R.id.search_writer)
+    }
+
+
+}
--- app/src/main/java/com/example/blackcows/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/blackcows/ui/search/SearchViewModel.kt
@@ -7,50 +7,64 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
-import com.example.blackcows.data.model.SearchCategoryDataSource
-import com.example.blackcows.data.repository.SearchCategoryRepository
-import com.example.blackcows.data.repository.SearchCategoryRepositoryImpl
+import com.example.blackcows.ListItem
 import com.example.blackcows.data.repository.VideoRepository
 import com.example.blackcows.data.repository.YoutubeRepositoryImpl
 import com.example.blackcows.network.RetrofitClient
+import com.example.blackcows.toVideoItem
 import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
 
-//class SearchViewModel(private val searchCategoryRepository: SearchCategoryRepository) : ViewModel() {
+private const val TAG = "SearchViewModel"
+
 class SearchViewModel(private val repository: VideoRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Search Fragment"
-    }
+    private val _trendingVideos = MutableLiveData<List<ListItem.VideoItem>?>()
+    val trendingVideos: LiveData<List<ListItem.VideoItem>?> = _trendingVideos
 
-    val text: LiveData<String> = _text
-
-//    fun getSearchSubCategory(searchCategory : SearchCategory) {
-//        searchCategoryRepository.getSearchSubCategory(searchCategory)
-//    }
-
-    fun getVideoThumbanail(){
+    fun fetchTrendingVideos(region: String = "KR"){
         viewModelScope.launch {
-            Log.d("SearchViewModel_data",repository.getTrendingVideos("").toString())
-            repository.getTrendingVideos("하드디스크")
+            runCatching {
+                val videos = repository.getTrendingVideos(region).items?.toVideoItem()
+                _trendingVideos.value = videos
+            }.onFailure {
+                Log.e(TAG, "fetchTrendingVideos() failed! : ${it.message}")
+                handleException(it)
+            }
         }
     }
 
+    private fun handleException(e: Throwable) {
+        when (e) {
+            is HttpException -> {
+                val errorJsonString = e.response()?.errorBody()?.string()
+                Log.e(TAG, "HTTP error: $errorJsonString")
+            }
+
+            is IOException -> Log.e(TAG, "Network error: $e")
+            else -> Log.e(TAG, "Unexpected error: $e")
+        }
+    }
+
+    // TODO 1. 검색 결과 출력 테스트를 먼저 진행
+    // TODO 1-1. 리싸이클러뷰 xml에 추가
+    // TODO 1-2. 리싸이클러뷰 아이템 만들기
+    // TODO 1-3. 리싸이클러뷰에 검색어를 출력하는걸 만들기
+
+    // TODO 2. 검색어를 입력하는 칸 제작
+    // TODO 3. 검색어를 빠르게 입력해주는 UI 제작
 }
 
-class SearchViewModelFactory : ViewModelProvider.Factory {
-//    private val searchCategoryRepository = SearchCategoryRepositoryImpl(SearchCategoryDataSource)
 
+class SearchViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(
         modelClass: Class<T>,
         extras: CreationExtras
     ): T {
-//        return SearchViewModel(
-//            searchCategoryRepository
-
          val repository = YoutubeRepositoryImpl(RetrofitClient.searchVideoRemoteDataSource)
         return SearchViewModel(
              repository
-
         ) as T
     }
 }
