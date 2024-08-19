a1b6a302aee3f2a84414b34e3da4bb2915935975
anna <a700hui@gmail.com>
2024-08-19 18:08:20 +0900

    feat : xml 수정, home 프래그먼트, 어댑터, 뷰모델 완성.

--- app/src/main/java/com/example/blackcows/data/repository/SearchCategoryRepositoryImpl.kt
+++ app/src/main/java/com/example/blackcows/data/repository/SearchCategoryRepositoryImpl.kt
@@ -2,10 +2,11 @@ package com.example.blackcows.data.repository
 
 import com.example.blackcows.data.model.SearchCategoryDataSource
 import com.example.blackcows.data.model.SearchSubCategory
+import com.example.blackcows.ui.home.HomeCategoryDataClass
 import com.example.blackcows.ui.search.SearchCategory
 
 class SearchCategoryRepositoryImpl(private val searchCategoryDataSource : SearchCategoryDataSource) : SearchCategoryRepository {
-    override fun getSearchSubCategory(searchCategoryType : SearchCategory): List<SearchSubCategory> {
+    override fun getSearchSubCategory(searchCategoryType: SearchCategory): List<SearchSubCategory> {
         return searchCategoryDataSource.getSearchSubCategory(searchCategoryType)
     }
 }
--- app/src/main/java/com/example/blackcows/ui/home/HomeViewPagerAdapter.kt
+++ app/src/main/java/com/example/blackcows/ui/home/HomeViewPagerAdapter.kt
@@ -6,28 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.PagerAdapter
+import com.example.blackcows.ListItem
 import com.example.blackcows.R
 
-// 화면 됐을 때 재생, 넘어가면 정지 -> 하지말라고 하심
-// my page에서 데이터 가져오기
-class HomeViewPagerAdapter (private val items : Array<Int>) : RecyclerView.Adapter<HomeViewPagerAdapter.PagerViewHolder>(){
+// 디테일 페이지에서 데이터 가져오기
+class HomeViewPagerAdapter (private var videos: List<ListItem.VideoItem>) : PagerAdapter(){
+    override fun getCount() = videos.size
 
+    override fun isViewFromObject(view: View, `object`: Any): Boolean = view == `object`
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ) = PagerViewHolder((parent))
-
-    override fun onBindViewHolder(holder: HomeViewPagerAdapter.PagerViewHolder, position: Int) {
-        // holder.itemsView.adapter =
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        super.destroyItem(container, position, `object`)
+        container.removeView(`object` as View)
     }
 
-    override fun getItemCount() = items.size
-
-    class PagerViewHolder(parent : ViewGroup) : RecyclerView.ViewHolder
-        (LayoutInflater.from(parent.context).inflate(R.layout.home_item_recyclerview, parent, false)){
-
-             val itemsView : RecyclerView = itemView.findViewById(R.id.home_viewPager)
-        }
+    fun ubdateVideos(newVideos : List<ListItem.VideoItem>) {
+        videos = newVideos
+        notifyDataSetChanged()
+    }
 
 }
